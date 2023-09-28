7abec0e272bc76d2c7f7463694ec8e535e058c6a
zar4za <zar4z4@yandex.ru>
2023-09-29 01:08:27 +0300

    fix: change exception ame

---
+++ Figures/Exceptions/ShapeException.cs
@@ -0,0 +1,11 @@
+﻿namespace Shapes.Exceptions;
+
+/// <summary>
+/// Base exception abstraction for Shapes
+/// </summary>
+public abstract class ShapeException : Exception
+{
+    protected ShapeException(string message) : base(message) { }
+
+    protected ShapeException(string message, Exception innerException) : base(message, innerException) { }
+}
