e99c994f21a4312104e0629b42bfa759c11e1c7a
zar4za <zar4z4@yandex.ru>
2023-09-29 00:57:49 +0300

    feat: add sql query

---
+++ ProductCategoryQuery.sql
@@ -0,0 +1,15 @@
+﻿/*
+Так как у нас связь Many To Many (M2M), у нас 3 исходных таблицы:
+Product - таблица с конкретными продуктами, их Id, названия, уникальный хар-ки и т.п.
+Category - таблица с категориями, их Id, названия, др. свойства присущие этой категории.
+ProductCategory - таблица для связи M2M, так мы присваиваем продуктам категории.
+
+Теперь выберем все комбинации [название продукта] [название категории] и выведем продукты, которым не присвоена категория
+*/
+
+SELECT p.Name, c.Name
+FROM Product as p
+FULL JOIN ProductCategory as pc
+ON p.ProductId = pc.ProductId
+FULL JOIN Category AS c
+ON pc.CategoryId = c.CategoryId
